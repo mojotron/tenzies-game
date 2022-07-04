343cbe6c0d871c977ab2eab7ddbbb144b4b6e495
mojotron <igormalj@gmail.com>
2022-07-04 07:29:30 +0200

    Refactor app component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import { useEffect, useState } from "react";
 import DiceGrid from "./components/DiceGrid";
+import { generateArray, generateRandomNumber } from "./helpers";
+import { nanoid } from "nanoid";
 
 const App = () => {
+  const [diceArr, setDiceArr] = useState([]);
+
+  const crateNewDiceArr = () => {
+    const generateNum1to6Inclusive = generateRandomNumber.bind(null, 1, 6);
+    const newDiceArr = generateArray(10, generateNum1to6Inclusive).map(
+      (num) => ({ id: nanoid(), value: num, filled: false })
+    );
+    setDiceArr(newDiceArr);
+  };
+
+  useEffect(() => {
+    crateNewDiceArr();
+  }, []);
+
   return (
     <div className="App">
-      <DiceGrid
-        dice={[
-          { id: 0, value: 4, filled: false },
-          { id: 1, value: 1, filled: true },
-          { id: 2, value: 2, filled: false },
-          { id: 3, value: 3, filled: false },
-          { id: 4, value: 4, filled: true },
-          { id: 5, value: 5, filled: false },
-          { id: 6, value: 6, filled: true },
-          { id: 7, value: 1, filled: true },
-          { id: 8, value: 2, filled: true },
-          { id: 9, value: 3, filled: true },
-        ]}
-      />
+      <DiceGrid dice={diceArr} />
+      <button type="button" onClick={crateNewDiceArr}>
+        Roll
+      </button>
     </div>
   );
 };
